0e6a87742f67e49298d534a45641116b75928fb1
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2020-09-13 13:00:15 -0400

    add line count 3E2

--- homework/HW1/HW1-final/P3.sh
+++ homework/HW1/HW1-final/P3.sh
@@ -3,4 +3,4 @@ grep [0-9] apollo13.txt | wc -l > apollo_out.txt
 grep --help | grep "\--count"
 ls *.py | wc
 find . -type f ! -perm -o=rw | wc -l
-find . -maxdepth 1 ! -perm -o=rw
+find . -maxdepth 1 ! -perm -o=rw | wc -l

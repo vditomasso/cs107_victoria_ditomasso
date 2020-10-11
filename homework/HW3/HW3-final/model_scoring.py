63d208fd1e1a3ab6c892efd3381fec1c629079d0
Victoria DiTomasso <victoriaditomasso@Victorias-MacBook-Pro.local>
2020-10-11 16:53:00 -0400

    print only best fitting params

--- homework/HW3/HW3-final/model_scoring.py
+++ homework/HW3/HW3-final/model_scoring.py
@@ -11,11 +11,20 @@ X_train, X_test, y_train, y_test = train_test_split(dataset['data'],dataset['tar
 alpha=0.1
 models = [OLS(alpha), RR(alpha)]
 
+scores=[]
+params = []
+
 for model in models:
     model.fit(X_train, y_train)
-    score = model.score(X_test, y_test)
-    best_params = model.get_params()
-    print('R2=',score,' params=',best_params)
+    scores.append(model.score(X_test, y_test))
+    params.append(model.get_params())
+
+print('Linear Regression R2=',scores[0],' Ridge Regression R2=',scores[1])
+
+if scores[0]<scores[1]:
+    print('Best params (linear) =',params[0])
+else:
+    print('Best params (ridge) =',params[1])
 
 #from sklearn import datasets
 #from sklearn.model_selection import train_test_split

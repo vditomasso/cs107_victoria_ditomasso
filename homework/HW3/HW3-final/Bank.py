e4bbc01b3ccc110752b548e7b002b63399cf131b
Victoria DiTomasso <victoriaditomasso@Victorias-MacBook-Pro.local>
2020-10-12 17:23:55 -0400

    Bank.py part B1

--- homework/HW3/HW3-final/Bank.py
+++ homework/HW3/HW3-final/Bank.py
@@ -27,7 +27,7 @@ class BankAccount():
         if amount<0:
             raise Exception('Cannot deposit a negative amount')
         else:
-            new_bal = self.balance + deposit
+            new_bal = self.balance + amount
             self.balance = new_bal
 
     def __str__(self):
@@ -35,4 +35,73 @@ class BankAccount():
     
     def __len__(self):
         return(int(self.balance))
+
+class BankUser():
+    
+    def __init__(self, owner):
+        self.owner = owner
+        self.account = BankAccount(owner, None)
+        self.savingsAccount = None
+        self.checkingAccount = None
+    
+    def addAccount(self, accountType):
+        if accountType == 1:
+            if self.savingsAccount == None:
+                self.savingsAccount = BankAccount(self.owner, accountType)
+            else:
+                raise Exception('This owner already has a savings account')
+        if accountType == 2:
+            if self.checkingAccount == None:
+                self.checkingAccount = BankAccount(self.owner, accountType)
+            else:
+                raise Exception('This owner already has a checking account')
+        
+    def getBalance(self, accountType):
+        if accountType == 1:
+            if self.savingsAccount == None:
+                raise Exception('This owner does not have a savings account')
+            else:
+                return(self.savingsAccount.balance)
+        if accountType == 2:
+            if self.checkingAccount == None:
+                raise Exception('This owner does not have a checking account')
+            else:
+                return(self.checkingAccount.balance)
+    
+    def deposit(self, accountType, amount):
+        if accountType == 1:
+            if self.savingsAccount == None:
+                raise Exception('This owner does not have a savings account')
+            else:
+                self.savingsAccount.deposit(amount)
+        if accountType == 2:
+            if self.checkingAccount == None:
+                raise Exception('This owner does not have a checking account')
+            else:
+                self.checkingAccount.deposit(amount)
+
+    def withdraw(self, accountType, amount):
+        if accountType == 1:
+            if self.savingsAccount == None:
+                raise Exception('This owner does not have a savings account')
+            else:
+                self.savingsAccount.withdraw(amount)
+        if accountType == 2:
+            if self.checkingAccount == None:
+                raise Exception('This owner does not have a checking account')
+            else:
+                self.checkingAccount.withdraw(amount)
+
+    def __str__(self):
+        owner_info = "{} ".format(self.owner)
+        if self.savingsAccount!=None:
+            savings_info = "has a savings account, balance: {} ".format(self.getBalance(1))
+        else:
+            savings_info = "has no savings account "
+        if self.checkingAccount != None:
+            checking_info = "and a checking account, balance: {} ".format(self.getBalance(2))
+        else:
+            checking_info = "and no checking account"
+        return(owner_info + savings_info + checking_info)
+        
 

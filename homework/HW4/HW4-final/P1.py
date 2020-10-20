82812c55b84c4e119b17c3b58a004e98f2029190
Victoria DiTomasso <victoriaditomasso@Victorias-MacBook-Pro.local>
2020-10-20 14:35:53 -0400

    add part C

--- homework/HW4/HW4-final/P1.py
+++ homework/HW4/HW4-final/P1.py
@@ -38,16 +38,19 @@ hs = [1e-1,1e-7,1e-15]
 f_FD_primes = []
 for h in hs:
     f_FD_primes.append(numerical_diff(f,h))
-names = [r'$h = 1\times10^{-1}$',r'$h = 1\times10^{-7}$',r'$h = 1\times10^{-15}$']
+names = [r'$f^\prime_{FD}(x)$ for $h = 1\times10^{-1}$',r'$f^\prime_{FD}(x)$ for $h = 1\times10^{-7}$',r'$f^\prime_{FD}(x)$ for $h = 1\times10^{-15}$']
 
 for f_FD_prime, name in zip(f_FD_primes,names):
     plt.plot(x, f_FD_prime(x), label=name)
 
-plt.plot(x, f_prime(x), label='Exact derivative',linestyle=':',linewidth=5,alpha=0.8,c='purple')
+plt.plot(x, f_prime(x), label='Analytical derivative',linestyle=':',linewidth=5,alpha=0.8,c='purple')
     
 plt.xlabel('x')
-plt.ylabel(r'$f^\prime_{FD}(x)$')
+plt.ylabel(r'$f^\prime(x)$')
 plt.legend()
-    
-plt.show()
+
+print('Answer to Q-a: When h=1e-7, the finite difference derivative most closely matches the true derivative. When the h value is too large, the differential is not fine enough to accurately probe the function\'s rate of change. When the h value is too small, the subtraction will have a large rounding error and the derivative will become numerically unstable.')
+print('Answer to Q-b: Automatic differentiaion bypasses both of these issues by breaking the input function into composite functions, finding the derivatives of those functions and combining them using the chain rule. In this way, AD calculates the true derivative to machine-precision.')
+
 plt.savefig('P1_fig.png')
+plt.show()

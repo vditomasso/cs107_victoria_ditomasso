cfce0e9c58130485b4f31811a22cf7e121373ede
Victoria DiTomasso <victoriaditomasso@Victorias-MacBook-Pro.local>
2020-10-28 11:09:51 -0400

    add animal.py, copied from instructions

---
+++ homework/HW5/HW5-final/animal.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+class Animal:
+
+    # a class attribute of the valid species in our universe
+    valid_species = {
+        'cat',
+        'dog',
+        'duck',
+        'elf',
+        'goblin',
+        'horse',
+        'human',
+        'mermaid',
+        'nightingale',
+        'pig',
+        'swan',
+        'wolf'
+    }
+
+    def __init__(self, name, species):
+        self.name = name
+        self._species = species
+
+    def __repr__(self):
+        return f'{self.name} ({self._species})'

3a98668e8712d85e5ebfb9ebf215079684847301
Victoria DiTomasso <victoriaditomasso@Victorias-MacBook-Pro.local>
2020-11-05 10:58:39 -0500

    add longest_sentence function

--- homework/HW5/HW5-final/longest_sentence.py
+++ homework/HW5/HW5-final/longest_sentence.py
@@ -1,5 +1,11 @@
 #!/bin/usr python3
 
+# Comment out these imports before submitting
+#import sys
+#sys.setrecursionlimit(2000)
+#from linked_list import Nil
+#from linked_list import LinkedList
+
 def get_list_of_sentences(chapter1='swansway-chapter1.txt'):
     def to_sentences(p):
             for delimiter in '.?!': p = p.replace(delimiter, '|')
@@ -17,5 +23,13 @@ def get_list_of_sentences(chapter1='swansway-chapter1.txt'):
 
 def longest_sentence():
     list_of_sentences = get_list_of_sentences()
+    
+    def count_words(string):
+        return len(string.split(' '))
+    sentence_lengths = list_of_sentences.for_each(count_words)
+    
+    def larger(a,b):
+        return b if b > a else a
+    return(sentence_lengths.reduce_right(larger))
 
-    pass # TODO
+

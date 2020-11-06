52c7c564897b14f3e6cb0a2eb61e93943f2e8a74
Victoria DiTomasso <victoriaditomasso@Victorias-MacBook-Pro.local>
2020-11-06 09:25:09 -0500

    add binary_search_tree with code from assignment

---
+++ homework/HW5/HW5-final/binary_search_tree.py
@@ -0,0 +1,50 @@
+#!/bin/usr python3
+
+class BSTNode:
+
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.left = None
+        self.right = None
+        self.size = 1
+
+    def __str__(self):
+        return f'BSTNode({self.key}, {self.val})' + \
+               '\n|\n|-(L)->' + '\n|      '.join(str(self.left ).split('\n')) + \
+               '\n|\n|-(R)->' + '\n|      '.join(str(self.right).split('\n'))
+
+
+class BSTTable:
+    def __init__(self):
+        self._root = None
+
+    def __str__(self):
+        return str(self._root)
+
+    def __len__(self):
+        return self._size(self._root)
+
+    def put(self, key, val):
+        self._root = self._put(self._root, key, val)
+
+    def get(self, key):
+        return self._get(self._root, key)
+
+    def _put(self, node, key, val):
+        pass # TODO
+
+    def _get(self, node, key):
+        pass # TODO
+
+    @staticmethod
+    def _size(node):
+        return node.size if node else 0
+
+### Testing ###
+
+greektoroman = BSTTable()
+greektoroman.put('Athena',    'Minerva')
+greektoroman.put('Eros',      'Cupid')
+greektoroman.put('Aphrodite', 'Venus')
+print(greektoroman.get('Eros'))
+print(greektoroman)

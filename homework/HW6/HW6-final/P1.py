c82ca05f1d67d195c565785cc9a3b13f96888d04
Victoria DiTomasso <victoriaditomasso@Victorias-MacBook-Pro.local>
2020-11-13 10:37:19 -0500

    add P1.py with code from assignment

---
+++ homework/HW6/HW6-final/P1.py
@@ -0,0 +1,56 @@
+!#/bin/usr python3
+
+class BSTNode:
+
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.left, self.right = None, None
+        self.size = 1
+
+    def __str__(self):
+        return f'BSTNode({self.key}, {self.val})' + \
+               '\n|\n|-(L)->' + '\n|      '.join(str(self.left ).split('\n')) + \
+               '\n|\n|-(R)->' + '\n|      '.join(str(self.right).split('\n'))
+
+
+class BSTTable:
+    def __init__(self):
+        self._root = None
+
+    def __str__(self):
+        return str(self._root)
+
+    def __len__(self):
+        return self._size(self._root)
+
+    def put(self, key, val):
+        self._root = self._put(self._root, key, val)
+
+    def get(self, key):
+        return self._get(self._root, key)
+
+    def _put(self, node, key, val):
+        if not node:
+            return BSTNode(key, val)
+        if   key < node.key:
+            node.left  = self._put(node.left,  key, val)
+        elif key > node.key:
+            node.right = self._put(node.right, key, val)
+        else:
+            node.val = val
+        node.size = 1 + self._size(node.left) + self._size(node.right)
+        return node
+
+    def _get(self, node, key):
+        if not node:
+            raise KeyError(f'key not found: {key}')
+        if   key < node.key:
+            return self._get(node.left,  key)
+        elif key > node.key:
+            return self._get(node.right, key)
+        else:
+            return node.val
+
+    @staticmethod
+    def _size(node):
+        return node.size if node else 0

44aecfb8829946154e3b10855903ff90c6534dd8
Victoria DiTomasso <victoriaditomasso@Victorias-MacBook-Pro.local>
2020-11-18 14:54:38 -0500

    starting to implement _remove method

--- homework/HW6/HW6-final/P1.py
+++ homework/HW6/HW6-final/P1.py
@@ -52,7 +52,7 @@ class BSTTable:
             return node.val
             
     def _removemin(self, node):
-        
+        '''I think it's ok that this can't handle when node.left.left does not exist, because that would mean the tree only has 1 node & what would it mean to remove the minimum node from a tree with only one node?'''
         if node.left.left is not None:
             node.size = node.size-1
             return(self._removemin(node.left))
@@ -61,14 +61,148 @@ class BSTTable:
             node.size = node.size-1
         return(self)
     
-#    def remove(self, key):
-#        self._root = self._remove(self._root, key)
+    def remove(self, key):
+        self._root = self._remove(self._root, key)
+
+    def _remove(self, node, key):
+        '''CANNOT HANDLE IF THE CURRENT NODE IS THE NODE THAT NEEDS TO BE DELETED
+        WOULD IT BE EASIER TO CREATE A SECOND METHOD THAT CAN FIND THE MAXIMUM VALUE IN THE LEFT SUBTREE?'''
+        # TODO: Should return a subtree whose root is  but without
+        #       the node whose key is
+        
+        if node.key < key:
+            node = node.left
+            return(self._remove(node, key))
+
+        elif node.key > key:
+            node = node.right
+            return(self._remove(node,key))
+            
+        elif node.key == key:
+            # if it has no children, just remove it
+            if node.size == 1:
+                node = None
+            # if it has one child, replace it with its child
+            elif node.size == 2:
+                if node.left is not None:
+                    node = node.left
+                elif node.right is not None:
+                    node = node.right
+                else:
+                    print('Neither node.left nor node.right are not None')
+            # if it has two chidren, replace it with the maximum value in the left subtree, and delete that node
+            elif node.size > 2:
+                # find maximum value in left subtree
+                node_max = node.right
+                while node_max.right is not None:
+                    node_max = node.max.right
+                # once you have the max node, replace the current node with it
+                node = node_max
+                # if that max node had a child on the left, replace it with that
+                if node_max.left is not None:
+                    node_max = node_max.left
+                # if not, replace the max node with none
+                else:
+                    node_max = None
+            else:
+                print('node.size is {}'.format(node.left.size))
+            return(self)
+            
+        else:
+            raise KeyError
+
+
+#########################################################
+### Not sure why I can't just write this for the current node. Why do I need to write it for the current node's children? ###
+#        # we want to remove the current node's left child
+#        try:
+#            if node.left.key==key:
+#                # if it has no children, just remove it
+#                if node.left.size == 1:
+#                    node.left = None
+#                # if it has one child, replace it with its child
+#                elif node.left.size == 2:
+#                    if node.left.left is not None:
+#                        node.left = node.left.left
+#                    elif node.left.right is not None:
+#                        node.left = node.left.right
+#                    else:
+#                        print('Neither node.left.left nor node.left.right are not None')
+#                # if it has two chidren, replace it with the maximum value in the left subtree, and delete that node
+#                elif node.left.size > 2:
+#                    # find maximum value in left subtree
+#                    node_max = node.left.right
+#                    while node_max.right is not None:
+#                        node_max = node.max.right
+#                    # once you have the max node, replace the current node's left child with it
+#                    node.left = node_max
+#                    # if that max node had a child on the left, replace it with that
+#                    if node_max.left is not None:
+#                        node_max = node_max.left
+#                    # if not, replace the max node with none
+#                    else:
+#                        node_max = None
+#                else:
+#                    print('node.left.size is {}'.format(node.left.size))
+#                return(self)
+#        except:
+#            pass
 #
-#    def _remove(self, node, key)
-#        # TODO: Should return a subtree whose root is  but without
-#        #       the node whose key is
-#        tree = BSTTable()
+#        # we want to remove the current node's right child
+#        try:
+#            if node.right.key==key:
+#                # if it has no children, just remove it
+#                if node.right.size == 1:
+#                    node.right = None
+#                # if it has one child, replace it with its child
+#                elif node.right.size == 2:
+#                    if node.right.left is not None:
+#                        node.right = node.right.left
+#                    elif node.right.right is not None:
+#                        node.right = node.right.right
+#                    else:
+#                        print('Neither node.right.left nor node.right.right are not None')
+#                # if it has two chidren, replace it with the maximum value in the left subtree, and delete that node
+#                elif node.right.size > 2:
+#                    # find maximum value in left subtree
+#                    node_max = node.right.right
+#                    while node_max.right is not None:
+#                        node_max = node.max.right
+#                    # once you have the max node, replace the current node's left child with it
+#                    node.right = node_max
+#                    # if that max node had a child on the left, replace it with that
+#                    if node_max.left is not None:
+#                        node_max = node_max.left
+#                    # if not, replace the max node with none
+#                    else:
+#                        node_max = None
+#                else:
+#                    print('node.right.size is {}'.format(node.left.size))
 #
+#        except:
+#            pass
+#
+#        # if we don't want to remove the current node's right or left child, then we need to keep looking for the key
+#        if node.key < key:
+#            node = node.left
+#            return(self._remove(node, key))
+#
+#        elif node.key > key:
+#            node = node.right
+#            return(self._remove(node,key))
+#
+#        else:
+#            raise KeyError
+            
+#######################################################
+
+#        # the current node is the node we want to remove
+#        if node.key == key:
+#        # if it has no children, just remove it
+#            if node.left is not None:
+                
+
+
 #        # the root is the node we want to remove
 #        if self._root.key == key:
 #            left_node = self._root.left
@@ -102,24 +236,28 @@ class BSTTable:
         return node.size if node else 0
         
 ### Testing for removemin ###
-t = BSTTable()
-t.put(5, 'a')
-t.put(1, 'b')
-t.put(2, 'c')
-t.put(0, 'd')
-print('t._root=\n',t._root)
-print('t._removemin(t._root)=\n',t._removemin(t._root))
-
-#### Testing for remove ###
-#
 #t = BSTTable()
 #t.put(5, 'a')
 #t.put(1, 'b')
 #t.put(2, 'c')
 #t.put(0, 'd')
-#
-#print(t)
-#
-#print(t._remove(t._root, 5))
-#
-#print(t._remove(t._remove(t._root, 5), 1))
+#print('t._root=\n',t._root)
+#print('t._removemin(t._root)=\n',t._removemin(t._root))
+
+#### Testing for remove ###
+
+t = BSTTable()
+t.put(5, 'a')
+t.put(1, 'b')
+t.put(2, 'c')
+t.put(0, 'd')
+
+print(t._root.size)
+
+print('t=\n',t)
+
+print('t._remove(t._root, 5)=\n',t._remove(t._root, 5))
+
+print('t._remove(t._remove(t._root, 5), 1)=\n',t._remove(t._remove(t._root, 5), 1))
+
+print('t._remove(t._root, 10)=\n',t._remove(t._root, 10))

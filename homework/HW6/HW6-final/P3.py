43359cda6529163fe8565a81da1d471494102851
Victoria DiTomasso <victoriaditomasso@Victorias-MacBook-Pro.local>
2020-11-22 13:59:16 -0500

    add PythonHeapPriorityQueue with methods put, get and peek

--- homework/HW6/HW6-final/P3.py
+++ homework/HW6/HW6-final/P3.py
@@ -3,6 +3,7 @@
 from random import sample
 from time import time
 import P2
+import heapq
 
 class PriorityQueue:
     def __init__(self, max_size):
@@ -109,6 +110,37 @@ class HeapPriorityQueue(PriorityQueue):
         else:
             raise IndexError('Queue is empty, cannot perform peek from an empty queue.')
             
+class PythonHeapPriorityQueue(PriorityQueue):
+    def __init__(self, max_size):
+        PriorityQueue.__init__(self, max_size)
+        
+    def put(self, val):
+    # put should push the element to the heap (heappush)
+    # raise an IndexError if called after max_size is reached. Nothing should be returned
+        if len(self)<self.max_size:
+            heapq.heapify(self.elements)
+            heapq.heappush(self.elements, val)
+        else:
+            raise IndexError('Queue is already at maximum length, cannot put another element.')
+
+    def get(self):
+    # get should remove and return the root element of the heap (heappop)
+    # raise an IndexError if called on an empty priority queue
+        if len(self)>0:
+            return(heapq.heappop(self.elements))
+        else:
+            raise IndexError('Queue is empty, cannot perform get on an empty queue.')
+            
+    def peek(self):
+    # peek should return the smallest value in the queue.
+    # raise an IndexError if called on an empty priority queue
+        if len(self)>0:
+            min_elem = heapq.heappop(self.elements)
+            heapq.heappush(self.elements,min_elem)
+            return(min_elem)
+        else:
+            raise IndexError('Queue is empty, cannot perform peek from an empty queue.')
+        
 def generatelists(n, length=20, dictionary_path='../data/words.txt'):
     with open(dictionary_path, 'r') as f:
         words = [w.strip() for w in f.readlines()]
@@ -135,13 +167,15 @@ def timeit(ns=(10, 20, 50, 100, 200, 500), pqclass=PriorityQueue, n_average=5):
 q = HeapPriorityQueue(2)
 q.put(1)
 q.put(2)
-print(q.elements)
+#print(q.elements)
 print(q.peek())
+#
+#print(q.elements)
 
 print(q.get())
 print(q.get())
 #print(q.elements)
-
+#
 print(q.get())
-
+#
 
